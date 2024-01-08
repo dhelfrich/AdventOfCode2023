986eff221a8521baf2085adf3b7f4a6775a4fe97
Daniel Helfrich <daniel.helfrich@colorado.edu>
2024-01-08 15:37:56 -0700

    day 17 part 1 working

--- Day17/day17.py
+++ Day17/day17.py
@@ -1,4 +1,5 @@
-import igraph 
+import igraph as ig
+import matplotlib as plt
 
 def dijkstra(grid, start):
     rows = len(grid)
@@ -101,6 +102,23 @@ def getNeighbors2(grid, prev, node):
         neighbors.append((node[0], node[1] + 1))
     return neighbors
 
+def igraphTest(grid, start):
+    rows = len(grid)
+    cols = len(grid[0])
+    dictdict = {}
+    for i in range(rows):
+        for j in range(cols):
+            neighbors = {}
+            for n in getNeighbors(grid, (i, j)):
+                neighbors[str(n)] = {'weight': int(grid[n[0]][n[1]])}
+            dictdict[str((i, j))] = neighbors
+    g = ig.Graph.DictDict(dictdict, directed=True)
+    ig.plot(g, vertex_label = g.vs["name"], target = './Day17/test.png')
+    result = g.distances("(0, 0)", ["({}, {})".format(rows-1, cols - 1), "(2, 2)"], weights='weight')
+    print(result)
+    return g
+
+
 def dijkstra3(grid, start):
     rows = len(grid)
     cols = len(grid[0])
@@ -109,66 +127,114 @@ def dijkstra3(grid, start):
     unvisited = []
     displacements = [(-3, 0), (-2, 0), (-1, 0), (1, 0), (2, 0), (3, 0),
                      (0, -3), (0, -2), (0, -1), (0, 1), (0, 2), (0, 3)]
-
+    dictdict = {}
     for i in range(rows):
         for j in range(cols):
             for t, (di, dj) in enumerate(displacements):
-                if i - di >= 0 and i - di < rows and j - dj >= 0 and j - dj < cols:
-                    dist[i][j][t] = 9999
-                    prev[i][j][t] = None
-                    unvisited.append(((i, j), (di, dj)))
-    
-    dist[start[0][0]][start[0][1]][displacements.index((start[1][0],start[1][1]))] = 0 #start as if coming from right below
+                neighbors = {}
+                for n in  getNeighbors3(grid, (i, j, t)):
+                    neighbors[str(n)] = {'weight': int(grid[n[0]][n[1]])}
+                dictdict[str((i, j, t))] = neighbors
+    g = ig.Graph.DictDict(dictdict, directed=True)
+    vs = g.vs["name"]
 
-    while unvisited != []:
-        u = unvisited[0]
-        tu = displacements.index((u[1][0], u[1][1]))
-        for i in range(rows):
-            for j in range(cols):
-                for t in range(12):
-                    di, dj = displacements[t]
-                    if ((i, j), (di, dj)) in unvisited and dist[i][j][t] < \
-                        dist[u[0][0]][u[0][1]][tu]:
-                            u = ((i, j), (di, dj))
-        unvisited.remove(u)
-
-        for n in getNeighbors3(grid, u):
-            if n in unvisited:
-                tn = displacements.index((n[1][0], n[1][1]))
-                tu = displacements.index((u[1][0], u[1][1]))
-                alt = dist[u[0][0]][u[0][1]][tu] + int(grid[n[0][0]][n[0][1]])
-                if alt < dist[n[0][0]][n[0][1]][tn]:
-                    dist[n[0][0]][n[0][1]][tn] = alt
-                    prev[n[0][0]][n[0][1]][tn] = u
-    return dist, prev
+    ig.plot(g, vertex_label = g.vs["name"], target = './Day17/test.png')
+    ends = ["({}, {}, {})".format(rows-1, cols - 1, i) for i in [3, 4, 5, 9, 10, 11]]
+    result = g.distances("(0, 0, 2)", ends, weights='weight')
+    paths = g.get_shortest_paths("(0, 0, 2)", ends, weights='weight')
+    paths2 = [[vs[v] for v in path] for path in paths]
+    return result, paths2
 
 def getNeighbors3(grid, node):
     rows = len(grid)
     cols = len(grid[0])
-    i, j = node[0]
-    di, dj = node[1]
+    i, j, t = node
     neighbors = []
-    if i > 0 and di != -3:
+    displacements = [(-3, 0), (-2, 0), (-1, 0), (1, 0), (2, 0), (3, 0),
+                     (0, -3), (0, -2), (0, -1), (0, 1), (0, 2), (0, 3)]
+    di, dj = displacements[t]
+    if i > 0 and di != -3 and di <= 0:
         if di < 0:
             neighbors.append(((i - 1, j), (di - 1, 0)))
         else:
             neighbors.append(((i - 1, j), (-1, 0)))
-    if i < rows - 1 and di != 3:
+    if i < rows - 1 and di != 3 and di >= 0:
         if di > 0:
             neighbors.append(((i + 1, j), (di + 1, 0)))
         else:
             neighbors.append(((i + 1, j), (1, 0)))
-    if j > 0 and dj != -3:
+    if j > 0 and dj != -3 and dj <= 0:
         if dj < 0:
             neighbors.append(((i, j - 1), (0, dj - 1)))
         else:
             neighbors.append(((i, j - 1), (0, -1)))
-    if j < cols - 1 and dj != 3:
+    if j < cols - 1 and dj != 3 and dj >= 0:
         if dj > 0:
             neighbors.append(((i, j + 1), (0, dj + 1)))
         else:
             neighbors.append(((i, j + 1), (0, 1)))
-    return neighbors
+    neighbors2 = []
+    for ((i, j), (di, dj)) in neighbors:
+        neighbors2.append((i, j, displacements.index((di, dj))))
+    return neighbors2
+
+def dijkstra4(grid, start):
+    rows = len(grid)
+    cols = len(grid[0])
+    dist = [[[None for t in range(12)] for j in range(cols)] for i in range(rows)]
+    prev = [[[None for t in range(12)] for j in range(cols)] for i in range(rows)]
+    unvisited = []
+    displacements = [(i, 0) for i in range(-10, 11) if i != 0] + [(0, j) for j in range(-10, 11) if j != 0]
+    dictdict = {}
+    for i in range(rows):
+        for j in range(cols):
+            for t, (di, dj) in enumerate(displacements):
+                neighbors = {}
+                for n in  getNeighbors4(grid, (i, j, t)):
+                    neighbors[str(n)] = {'weight': int(grid[n[0]][n[1]])}
+                dictdict[str((i, j, t))] = neighbors
+    g = ig.Graph.DictDict(dictdict, directed=True)
+    vs = g.vs["name"]
+
+    ig.plot(g, vertex_label = g.vs["name"], target = './Day17/test.png')
+    ends = ["({}, {}, {})".format(rows-1, cols - 1, i) for i in [3, 4, 5, 9, 10, 11]]
+    result = g.distances("(0, 0, 2)", ends, weights='weight')
+    paths2 = []
+    # paths = g.get_shortest_paths("(0, 0, 2)", ends, weights='weight')
+    # paths2 = [[vs[v] for v in path] for path in paths]
+    return result, paths2
+
+def getNeighbors4(grid, node):
+    rows = len(grid)
+    cols = len(grid[0])
+    i, j, t = node
+    neighbors = []
+    displacements = [(i, 0) for i in range(-10, 11) if i != 0] + [(0, j) for j in range(-10, 11) if j != 0]
+    di, dj = displacements[t]
+    if i > 0 and di != -3 and di <= 0:
+        if di < 0:
+            neighbors.append(((i - 1, j), (di - 1, 0)))
+        else:
+            neighbors.append(((i - 1, j), (-1, 0)))
+    if i < rows - 1 and di != 3 and di >= 0:
+        if di > 0:
+            neighbors.append(((i + 1, j), (di + 1, 0)))
+        else:
+            neighbors.append(((i + 1, j), (1, 0)))
+    if j > 0 and dj != -3 and dj <= 0:
+        if dj < 0:
+            neighbors.append(((i, j - 1), (0, dj - 1)))
+        else:
+            neighbors.append(((i, j - 1), (0, -1)))
+    if j < cols - 1 and dj != 3 and dj >= 0:
+        if dj > 0:
+            neighbors.append(((i, j + 1), (0, dj + 1)))
+        else:
+            neighbors.append(((i, j + 1), (0, 1)))
+    neighbors2 = []
+    for ((i, j), (di, dj)) in neighbors:
+        neighbors2.append((i, j, displacements.index((di, dj))))
+    return neighbors2
 def getPrev(grid, prev, node):
     displacements = [(-3, 0), (-2, 0), (-1, 0), (1, 0), (2, 0), (3, 0),
                      (0, -3), (0, -2), (0, -1), (0, 1), (0, 2), (0, 3)]
@@ -186,17 +252,38 @@ def drawPath(grid, prev):
         cur = getPrev(grid, prev, cur)
     return path
 
+def drawPath2(grid, path):
+    rows = len(grid)
+    cols = len(grid)
+    pathPrint = [[grid[i][j] for j in range(cols)] for i in range(rows)]
+    for p in path:
+        v = eval(p)
+        pathPrint[v[0]][v[1]] = '#'
+    for line in pathPrint:
+        print(*line)
+
+def toDisplacement(t):
+    displacements = [(-3, 0), (-2, 0), (-1, 0), (1, 0), (2, 0), (3, 0),
+                     (0, -3), (0, -2), (0, -1), (0, 1), (0, 2), (0, 3)]
+    return displacements[t]
 
 
 def main():
     data = open("./Day17/day17.txt").read()  # read the file
-    data = open("./Day17/day17Sample.txt").read()  # read the file
+    # data = open("./Day17/day17Sample.txt").read()  # read the file
     grid = data.split("\n")  # split the file into a list of words
-    dist, prev = dijkstra3(grid, ((0,0), (-1, 0)))
-    for i in range(len(dist)):
-        print(dist[i])
-    path = drawPath(grid, prev)
-    for i in range(len(dist)):
-        print(*path[i], '\t')
+    # gridTest = [line[:4] for line in grid[:4]]
+    # print(getNeighbors3(grid, (0, 0, 3)))
+    # g = igraphTest(grid, (0,0 ))
+    # results, paths = dijkstra3(grid, (0, 0, 2))
+    # print(results)
+    # for i, p in enumerate(paths):
+    #     print(i, results[0][i])
+    #     drawPath2(grid, p)
+    
+    # for p in paths[2]:
+    #     v = eval(p)
+    #     print(v[0], v[1], toDisplacement(v[2]))
+    dijkstra4(grid, (0, 0, 2))
 
 main()
